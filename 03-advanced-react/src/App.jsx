27ff11ced414d8de4c8cc6dc291c6c7ac5be7587
duycoding <128799277+duycoding@users.noreply.github.com>
2024-07-20 22:55:34 +0700

    comple about form

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -10,11 +10,13 @@ import FetchData from './tutorial/02-useEffect/starter/04-fetch-data';
 import ControlledInputs from './tutorial/06-forms/starter/01-controlled-inputs';
 import UserChallenge from './tutorial/06-forms/starter/02-user-challenge';
 import MultipleEffects from './tutorial/06-forms/starter/03-multiple-inputs';
+import OtherInputs from './tutorial/06-forms/starter/04-other-inputs';
+import UncontrolledInputs from './tutorial/06-forms/starter/05-form-data';
 
 function App() {
 	return (
 		<div className='container'>
-			<MultipleEffects />
+			<UncontrolledInputs />
 		</div>
 	);
 }
--- 03-advanced-react/src/tutorial/06-forms/starter/04-other-inputs.jsx
+++ 03-advanced-react/src/tutorial/06-forms/starter/04-other-inputs.jsx
@@ -1,24 +1,66 @@
 import { useState } from 'react';
 const frameworks = ['react', 'angular', 'vue', 'svelte'];
 const OtherInputs = () => {
-  return (
-    <div>
-      <form className='form'>
-        <h4>Other Inputs</h4>
-        {/* name */}
-        <div className='form-row' style={{ textAlign: 'left' }}>
-          <label htmlFor='shipping'> Free Shipping </label>
-        </div>
-        <div className='form-row' style={{ textAlign: 'left' }}>
-          <label htmlFor='framework' className='form-label'>
-            Framework
-          </label>
-        </div>
-        <button type='submit' className='btn btn-block'>
-          submit
-        </button>
-      </form>
-    </div>
-  );
+	const [shipping, setShipping] = useState(false);
+	const [framework, setFramework] = useState('react');
+
+	const handleShipping = (e) => {
+		console.log(shipping);
+		setShipping(e.target.checked);
+	};
+
+	const handleSelect = (e) => {
+		console.log(framework);
+		setFramework(e.target.value);
+	};
+
+	console.log(framework, shipping);
+
+	return (
+		<div>
+			<form
+				className='form'
+				onSubmit={(e) => {
+					e.preventDefault();
+				}}>
+				<h4>Other Inputs</h4>
+				{/* name */}
+				<div
+					className='form-row'
+					style={{ textAlign: 'left' }}>
+					<input
+						type='checkbox'
+						checked={shipping}
+						name='shipping'
+						onChange={handleShipping}
+					/>
+					<label htmlFor='shipping'> Free Shipping </label>
+				</div>
+				<div
+					className='form-row'
+					style={{ textAlign: 'left' }}>
+					<label
+						htmlFor='framework'
+						className='form-label'>
+						Framework
+					</label>
+					<select
+						name='framework'
+						id='framework'
+						value={framework}
+						onChange={handleSelect}>
+						{frameworks.map((item) => {
+							return <option key={item}>{item}</option>;
+						})}
+					</select>
+				</div>
+				<button
+					type='submit'
+					className='btn btn-block'>
+					submit
+				</button>
+			</form>
+		</div>
+	);
 };
 export default OtherInputs;
