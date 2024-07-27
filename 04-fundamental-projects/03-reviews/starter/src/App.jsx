d166a656dba2a77f5e07d48eed2fd08a6d8d6743
duycoding <128799277+duycoding@users.noreply.github.com>
2024-07-27 16:01:18 +0700

    03-reviews

--- 04-fundamental-projects/03-reviews/starter/src/App.jsx
+++ 04-fundamental-projects/03-reviews/starter/src/App.jsx
@@ -1,4 +1,70 @@
+import { FaBeer, FaIndent } from 'react-icons/fa';
+import reviews from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import { useState } from 'react';
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+	const [index, setIndex] = useState(0);
+
+	const handlePre = () => {
+		setIndex((prevIndex) => {
+			const newIndex = prevIndex - 1;
+			return newIndex < 0 ? reviews.length - 1 : newIndex;
+		});
+	};
+
+	const handleNext = () => {
+		setIndex((prevIndex) => {
+			const newIndex = prevIndex + 1;
+			return newIndex >= reviews.length ? 0 : newIndex;
+		});
+	};
+
+	const randomPersion = () => {
+		let randomNumber = Math.floor(Math.random() * reviews.length);
+		if (randomNumber === index) randomNumber = index + 1;
+		const newIndex = randomNumber % reviews.length;
+		setIndex(newIndex);
+	};
+
+	return (
+		<main>
+			<article className='review'>
+				<div key={reviews[index].id}>
+					<div className='img-container'>
+						<img
+							src={reviews[index].image}
+							alt={reviews[index].name}
+							className='person-img'
+						/>
+						<span className='quote-icon'>
+							<FaQuoteRight />
+						</span>
+					</div>
+
+					<h4>{reviews[index].name}</h4>
+					<p className='job'>{reviews[index].job}</p>
+					<div className='btn-container'>
+						<button
+							className='prev-btn'
+							onClick={handlePre}>
+							<FaChevronLeft />
+						</button>
+						<button
+							className='next-btn'
+							onClick={handleNext}>
+							<FaChevronRight />
+						</button>
+					</div>
+				</div>
+				<div
+					className='btn btn-hipster'
+					onClick={randomPersion}>
+					supprise me
+				</div>
+			</article>
+		</main>
+	);
 };
+
 export default App;
